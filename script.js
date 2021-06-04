2fc0ac3fcb5ffce64a7eabdc27bfeb48eaebeabe
hbeck1115 <45074163+hbeck1115@users.noreply.github.com>
2021-06-04 12:41:32 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-// the function compute() is executed when the control 'Compute Interest' button is clicked.
-// purpose: this function read the inputs principal, interest rate, and the year from the html elements and displays the result on the 'Simple Interest Calculator' UI
-// a validation alert will popup on the widow when the principal value is 0 or negative number
+/ the function compute() is executed when the control 'Compute Interest' button is clicked.
+// purpose: this function takes the inputs principal, interest rate, and the number of years from the html elements, and the result is calculated and displayed on the 'Simple Interest Calculator' UI.
+// the validation alert is shown on the browser when the principal value is 0 or negative number
 function compute()
 {
     var result = "";
@@ -15,16 +15,16 @@ function compute()
         document.getElementById("principal").focus();
     }
     else{
-        result = "If you deposit \<mark\> " + principal + "\</mark\>" + ",\<br\> at an interest rate of \<mark\>" + rate + "%\</mark\> \<br\>" 
-        + "You will receive an amount of \<mark\>" + interest + ",\</mark\> \<br\>" 
+        result = "If you deposit \<mark\> " + principal + "\</mark\>" + "\<br\> at an interest rate of \<mark\>" + rate + "%\</mark\> \<br\>" 
+        + "You will receive an amount of \<mark\>" + interest + " \</mark\> \<br\>" 
         + "in the year \<mark\>" + year + "\</mark\> \<br\>";
         document.getElementById("result").innerHTML = result;
     }
 }
 
-// purpose: function updateRate() displays the result of the sliding control 'Interest Rate' on UI
+// purpose: function updateRate() displays the percent interest rate result of the sliding control on UI
 function updateRate()
 {
     var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText = rateval;
+    document.getElementById("rate_val").innerText = rateval  + " %";
 }
